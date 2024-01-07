e1e1624e25beee8ffc04f8f110cafe2a219ed835
JeongTaeGo <giwigi@naver.com>
2024-01-07 17:57:18 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SubPage Example',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/',
+      routes: {'/': (context) => FirstPage(),
+    '/second': (context) => SecondPage()},
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub Page Main'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('첫 번째 페이지'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).pushNamed('/second');  //push 대신에 pushNamed로 route의 경로를 이용하기
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: (){},
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
+    );
+  }
+}
